3803f7d41960fea14c6488c2a168df3044f42690
Abhay Singh <abhaysingh9@hotmail.com>
2018-04-19 22:41:55 -0700

    Bug free.
    Exact implementation. Readme to be updated.

--- buildngramtree.m
+++ buildngramtree.m
@@ -2,29 +2,22 @@
 function [ngtree] = buildngramtree(txt,n)
     ngtree = tree;
     for i = 1 : strlength(txt)
-        countfirst =  count(txt,txt(i));
         flag = exists(txt(i),ngtree,1);
         if(~flag)
-            ent = count(txt,txt(i))/countfirst;
-            
-            node = [txt(i) , {count(txt,txt(i))} , {ent} ];
+            node = [txt(i) , {count(txt,txt(i))},{1},{0}];
             ngtree = ngtree.addnode(1,node);
         end
-        entnode = ngtree.get(getindex(txt(i),ngtree,1));
-        ent = entnode{3};
         index = getindex(txt(i),ngtree,1);
-        
         for j = 2 : min(n,strlength(txt)-i+1)
             flagsub = exists(txt(j+i-1),ngtree,index);
             if(~flagsub)
-                ent = ent * count(txt,txt(i:i+j-1));
-                node = [txt(i+j-1) , {count(txt,txt(i:i+j-1))}, {ent} ];
+                node = [txt(i+j-1) , {count(txt,txt(i:i+j-1))},{j},{0}];
                 ngtree = ngtree.addnode(index,node);
             end
             index = getindex(txt(i+j-1),ngtree, index);
         end
     end
-    ngtree.tostring;
+    ngtree = entr(ngtree,n);
 end
 
 
@@ -51,3 +44,61 @@ function [index] = getindex(check,ngtree,ind)
         end
     end
 end
+
+%% compute entropy
+function [ngtre] = entr(ngtree,n)
+    it = ngtree.breadthfirstiterator;    
+    for i = length(it):-1:2
+        current = ngtree.get(it(i));
+        if(ngtree.getparent(it(i))~=1)
+            parent = ngtree.get(ngtree.getparent(it(i)));
+            parent{4} = parent{4} - log2(current{2}/parent{2})*current{2}/parent{2};
+            ngtree = ngtree.set(ngtree.getparent(it(i)), parent);
+        end
+    end
+    ngtre = standardize(ngtree,n);
+end
+
+%% standardize levelwise
+function [ngt] = standardize(ngtree,n)
+    children = ngtree.getchildren(1);
+    meanvector = zeros(2,n);
+    stdvector = zeros(2,n);
+    for i = 1:n
+        nextlayer = [];
+        elements = zeros(2,length(children));
+        for j = 1: length(children)
+            current = ngtree.get(children(j));
+            elements(1,j) = current{2};
+            elements(2,j) = current{4};
+            nextlayer = [nextlayer, ngtree.getchildren(children(j))];
+        end
+        meanvector(1,i) = mean(elements(1,:));
+        stdvector(1,i) = std(elements(1,:));
+        meanvector(2,i) = mean(elements(2,:));
+        stdvector(2,i) = std(elements(2,:));
+        children = nextlayer;
+    end
+    it = ngtree.breadthfirstiterator;    
+    for i = it
+        if(i~=1)
+            current = ngtree.get(i);
+            lvl = current{3};
+            current{2} = abs(current{2} - meanvector(1,lvl))/stdvector(1,lvl);
+            current{4} = abs(current{4} - meanvector(2,lvl))/stdvector(2,lvl);
+            ngtree = ngtree.set(i,current);
+        end
+    end
+    ngt = ngtree;
+end
+
+
+
+
+
+
+
+
+
+
+
--- votingexperts.m
+++ votingexperts.m
@@ -1,8 +1,8 @@
 %% Discrete data voting experts
-function [segregated] = votingexperts(input,checking,n)
+function [segregated] = votingexperts(input,checking,n,cutoff)
 txt = char(lower(importdata(input)));
 txtchk = char(lower(importdata(checking)));
 ngramtree = buildngramtree(txt,n);
-segregated = expertsvote(ngramtree,txt,n)
+segregated = expertsvote(ngramtree,txt,n,cutoff)
 txtchk
 %ourput the result
